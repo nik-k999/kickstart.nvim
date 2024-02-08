c0e46085d8a7aca35f6f7d083c9ffa0418411645
Nik <57092040+nik-k999@users.noreply.github.com>
2024-02-08 14:42:18 +0100

    cargo clippy

--- init.lua
+++ init.lua
@@ -592,7 +592,13 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  rust_analyzer = {},
+  rust_analyzer = {
+    ["rust-analyzer"] = {
+      check = {
+        command = "clippy",
+      },
+    },
+  },
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
