56eed47f2c7f9e7240d59e1294a3fb6f9e84a46a
Nik Kiene <nik.kiene.76@gmail.com>
2024-05-22 10:48:08 +0200

    disable copilot, enable toggle term, add makdown

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,22 +1,21 @@
--- function setTermMapping()
--- 	if vim.fn.has('macunix') == 1 then
--- 		return [[˙]]
--- 	else
--- 		return [[<A-h>]]
--- 	end
--- end
---
--- local term_mapping = setTermMapping()
---
--- return {
--- 	{
--- 		'akinsho/toggleterm.nvim',
--- 		config = function()
--- 			require("toggleterm").setup({
--- 				open_mapping = term_mapping,
--- 				size = 30,
--- 			})
--- 		end,
--- 	},
--- }
-return {}
+function SetTermMapping()
+  if vim.fn.has 'macunix' == 1 then
+    return [[˙]]
+  else
+    return [[<A-h>]]
+  end
+end
+
+local term_mapping = SetTermMapping()
+
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    config = function()
+      require('toggleterm').setup {
+        open_mapping = term_mapping,
+        size = 30,
+      }
+    end,
+  },
+}
