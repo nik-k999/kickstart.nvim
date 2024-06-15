2272adfe32a79f986957045bae773d39c48319f1
Nik Kiene <nik.kiene.76@gmail.com>
2024-06-15 09:48:15 +0200

    add ufo

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,8 +1,10 @@
 return {
-	"mbbill/undotree",
-	init = function()
-		vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-		vim.opt.undofile = true
-		vim.keymap.set("n", "<leader>u", function() vim.cmd("UndotreeToggle") end)
-	end,
+  'mbbill/undotree',
+  init = function()
+    vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+    vim.opt.undofile = true
+    vim.keymap.set('n', '<leader>u', function()
+      vim.cmd 'UndotreeToggle'
+    end)
+  end,
 }
--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -1,5 +1,11 @@
 vim.wo.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.cursorline = true
-vim.opt.colorcolumn = "80"
-vim.cmd.colorscheme "catppuccin-latte"
+vim.opt.colorcolumn = '80'
+vim.cmd.colorscheme 'catppuccin-latte'
+
+-- fold
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
